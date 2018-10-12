8f48ec5e425b50ac0251180f8b064ab936ab0109
Lioman <dev@lioman.de>
2018-10-12 08:33:25 +0200

    add python versions to setup.cfg

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements,
